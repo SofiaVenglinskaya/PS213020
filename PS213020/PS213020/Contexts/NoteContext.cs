3b0fccc8baa1cad75cb9047eadae959fc07d9bec
yourboyfriendsdrug <74487159+yourboyfriendsdrug@users.noreply.github.com>
2021-10-02 17:55:27 +0300

    что то сделали

---
+++ PS213020/PS213020/Contexts/NoteContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PS213020.Models;
+
+namespace PS213020.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+        
+
+    
+
+    }
+}
---
+++ PS213020/PS213020/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS213020.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? changeDate { get; set; }
+
+    }
+}
